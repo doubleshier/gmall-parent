e5045104a42c91e77b4aabb434cacbd7777db2f4
王十二 <1229529947@qq.com>
2022-08-23 19:58:38 +0800

    项目初始化

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/BaseCategory3Service.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.product.service;
+
+import com.atguigu.gmall.model.product.BaseCategory3;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
+/**
+* @author 王
+* @description 针对表【base_category3(三级分类表)】的数据库操作Service
+* @createDate 2022-08-23 00:24:20
+*/
+public interface BaseCategory3Service extends IService<BaseCategory3> {
+
+    List<BaseCategory3> getCategory2Child(Long c2Id);
+}
---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/BaseCategory3ServiceImpl.java
@@ -0,0 +1,36 @@
+package com.atguigu.gmall.product.service.impl;
+
+import com.atguigu.gmall.model.product.BaseCategory3;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import com.atguigu.gmall.product.service.BaseCategory3Service;
+import com.atguigu.gmall.product.mapper.BaseCategory3Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+* @author 王
+* @description 针对表【base_category3(三级分类表)】的数据库操作Service实现
+* @createDate 2022-08-23 00:24:20
+*/
+@Service
+public class BaseCategory3ServiceImpl extends ServiceImpl<BaseCategory3Mapper, BaseCategory3>
+    implements BaseCategory3Service{
+    @Autowired
+        BaseCategory3Mapper baseCategory3Mapper;
+    @Override
+    public List<BaseCategory3> getCategory2Child(Long c2Id) {
+        QueryWrapper<BaseCategory3> queryWrapper=new QueryWrapper<>();
+        queryWrapper.eq("category2_id",c2Id);
+        List<BaseCategory3> baseCategory3s = baseCategory3Mapper.selectList(queryWrapper);
+
+        return  baseCategory3s;
+    }
+}
+
+
+
+
